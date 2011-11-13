345a3bff37d1342c9233ec426d6e54754895dfe3
sameerhiware <sameerhiware@localhost>
2011-11-13 10:42:35 +0530

    file merged :)

--- Read_Code.py
+++ Read_Code.py
@@ -3,25 +3,31 @@ from decoder import *
 from Baseclass import *
 from pylab import *
 
-def Read_Code():
-	f=open('test.asm')
-	asm_code=f.read()
-	asm_code_list=asm_code.splitlines()
-	print asm_code
-	f.close()
+f=open('test.asm')
+asm_code=f.read()
+print asm_code
+asm_code_list=asm_code.strip()
+#print asm_code_list
+asm_code_list=asm_code_list.splitlines()
+print asm_code_list
 
-	opcode=[]
+f.close()
+opcode=[]
 
-	for n in asm_code_list:
+count=0
+for n in asm_code_list:
+	if (asm_code_list[count]==''):
+		pass
+	else:
 		opcode+=decode(n)
 
-	print opcode
+	count=count+1
+print opcode
 
-	for n in range(0,len(opcode)):
-		UC.ROM[n]=opcode[n]
+for n in range(0,len(opcode)):
+	UC.ROM[n]=opcode[n]
 
-	UC.PC=len(opcode)
-	print UC.ROM[0:20]
-	print UC.PC
+UC.PC=len(opcode)
+print UC.ROM[0:20]
+print UC.PC
 
-### Check
