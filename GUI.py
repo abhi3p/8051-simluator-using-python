4eb6ab56c9e3dd8720751a04915c0a2a3b7d18e1
Abhishek Pandey <abhi.pandey03@iitb.ac.in>
2011-11-15 18:53:42 +0530

    Modified GUI and updated main.py now just run main.py u can see GUI

--- GUI.py
+++ GUI.py
@@ -6,165 +6,169 @@ from Baseclass import *
 from decoder import *
 
 ######## GUI FILE #############
-
-class MyApp:
-	def __init__(self, parent):
+def GUI():
+	class MyApp:
+		def __init__(self, parent):
 		
-		self.myParent = parent 
-		self.myContainer1 = Frame(parent) 
-		self.myContainer1.pack()
+			self.myParent = parent 
+			self.myContainer1 = Frame(parent) 
+			self.myContainer1.pack()
 	
-		# top frame
-		self.top_frame = Frame(self.myContainer1) 
-		self.top_frame.pack(side=TOP,
-			fill=BOTH, 
-			expand=YES,
-			)  
+			# top frame
+			self.top_frame = Frame(self.myContainer1) 
+			self.top_frame.pack(side=TOP,
+				fill=BOTH, 
+				expand=YES,
+				)  
 
-		#bottom frame
-		self.bottom_frame = Frame(self.myContainer1, 
-			borderwidth=5,  relief=RIDGE,
-			height=500, 
-			background="white",
-			)   
-		self.bottom_frame.pack(side=TOP,
-			fill=BOTH, 
-			expand=YES,
-			)  
+			#bottom frame
+			self.bottom_frame = Frame(self.myContainer1, 
+				borderwidth=5,  relief=RAISED,
+				height=500, 
+				background="gray",
+				)   
+			self.bottom_frame.pack(side=TOP,
+				fill=BOTH, 
+				expand=YES,
+				)  
 
 				
-		# left_frame        
-		self.left_frame = Frame(self.top_frame, background="white",
-			borderwidth=5,  relief=RIDGE,
-			height=500, 
-			width=450, 
+			# left_frame        
+			self.left_frame = Frame(self.top_frame, background="gray",
+				borderwidth=5,  relief=RAISED,
+				height=500, 
+				width=465, 
+				)
+			self.left_frame.pack(side=LEFT,
+				fill=BOTH, 
+				expand=YES,
+				)  
+
+
+			### right_frame 
+			self.right_frame = Frame(self.top_frame, background="gray",
+				borderwidth=5,  relief=RAISED,
+				width=465,
+				)
+			self.right_frame.pack(side=RIGHT,
+				fill=BOTH, 
+				expand=YES,
+				)  
+
+			### center_frame 
+			self.center_frame = Frame(self.top_frame, background="gray",
+				borderwidth=5,  relief=RAISED,
+				width=465,
+				)
+			self.center_frame.pack(side=RIGHT,
+				fill=BOTH, 
+				expand=YES,
+				)  		
+
+			### Left Frame Contents: Assembly Language Code" 
+			fixedFont = Pmw.logicalfont('Times', sizeIncr=-1, weight='bold')
+			self.st = Pmw.ScrolledText(self.left_frame,
+			labelpos = 'n',
+			label_text='Assembly Language Code',
+			label_font=('Times', 15),
+			
+			usehullsize = 1,
+			hull_width = 465,
+			hull_height = 500,
+			text_wrap='none',
+			text_font = fixedFont,
+
+			text_padx = 4,
+			text_pady = 4,
 			)
-		self.left_frame.pack(side=LEFT,
-			fill=BOTH, 
-			expand=YES,
-			)  
-
-
-		### right_frame 
-		self.right_frame = Frame(self.top_frame, background="white",
-			borderwidth=5,  relief=RIDGE,
-			width=450,
-			)
-		self.right_frame.pack(side=RIGHT,
-			fill=BOTH, 
-			expand=YES,
-			)  
-
-		### center_frame 
-		self.center_frame = Frame(self.top_frame, background="white",
-			borderwidth=5,  relief=RIDGE,
-			width=450,
-			)
-		self.center_frame.pack(side=RIGHT,
-			fill=BOTH, 
-			expand=YES,
-			)  		
-
-		### Left Frame Contents: Assembly Language Code" 
-		fixedFont = Pmw.logicalfont('Fixed')
-		self.st = Pmw.ScrolledText(self.left_frame,
-		labelpos = 'n',
-		label_text='Assembly Language Code',
-
-
-		usehullsize = 1,
-		hull_width = 450,
-		hull_height = 500,
-		text_wrap='none',
-		text_font = fixedFont,
-
-		text_padx = 4,
-		text_pady = 4,
-		)
-		self.st.importfile('test.asm');
-		self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
-
-        	# Prevent users' modifying text and headers
-		self.st.configure(
-            	text_state = 'disabled',
-
-        	)				
-
-		### Right Frame Frame Contents: Registers and Memory Contents" 
-		fixedFont = Pmw.logicalfont('Fixed')
-		self.st = Pmw.ScrolledText(self.right_frame,
-		labelpos = 'n',
-		label_text='Registers & Memory Contents',
-
-		usehullsize = 1,
-		hull_width = 450,
-		hull_height = 500,
-		text_wrap='none',
-		text_font = fixedFont,
-
-		text_padx = 4,
-		text_pady = 4,
-		)
-		self.st.importfile('register.txt');
-		self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
-
-        	# Prevent users' modifying text and headers
-		self.st.configure(
-            	text_state = 'disabled',
-
-        	)
-
-
-		### Center Frame Contents: Registers and Memory Contents" 
-		fixedFont = Pmw.logicalfont('Fixed')
-		self.st = Pmw.ScrolledText(self.center_frame,
-		labelpos = 'n',
-		label_text='Program Memory & Opcodes',
-
-		usehullsize = 1,
-		hull_width = 450,
-		hull_height = 500,
-		text_wrap='none',
-		text_font = fixedFont,
-
-		text_padx = 4,
-		text_pady = 4,
-		)
-		self.st.importfile('opcode_write.txt');
-		self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
-
-        	# Prevent users' modifying text and headers
-		self.st.configure(
-            	text_state = 'disabled',
-
-        	)
-		
-		### Bottom Frame Contents: Registers and Memory Contents" 
-		fixedFont = Pmw.logicalfont('Fixed')
-		self.st = Pmw.ScrolledText(self.bottom_frame,
-		labelpos = 'n',
-		label_text='Others',
-
-		usehullsize = 1,
-		hull_width = 1400,
-		hull_height = 500,
-		text_wrap='none',
-		text_font = fixedFont,
-
-		text_padx = 4,
-		text_pady = 4,
-		)
-		self.st.importfile('test.asm');
-		self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
-
-        	# Prevent users' modifying text and headers
-		self.st.configure(
-            	text_state = 'disabled',
-
-        	)
+			self.st.importfile('test.asm');
+			self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
 			
 
-root = Tk()
-root.title('8051 Simulator')
-myapp = MyApp(root)
-root.mainloop()
+			# Prevent users' modifying text and headers
+			self.st.configure(
+		    	text_state = 'disabled',
+
+			)				
+
+			### Right Frame Frame Contents: Registers and Memory Contents" 
+			fixedFont = Pmw.logicalfont('Times', sizeIncr=-1, weight='bold')
+			self.st = Pmw.ScrolledText(self.right_frame,
+			labelpos = 'n',
+			label_text='Registers & Memory Contents',
+			label_font=('Times', 15),
+
+			usehullsize = 1,
+			hull_width = 465,
+			hull_height = 500,
+			text_wrap='none',
+			text_font = fixedFont,
+
+			text_padx = 4,
+			text_pady = 4,
+			)
+			self.st.importfile('register.txt');
+			self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
+
+			# Prevent users' modifying text and headers
+			self.st.configure(
+		    	text_state = 'disabled',
+
+			)
+
+
+			### Center Frame Contents: Registers and Memory Contents" 
+			fixedFont = Pmw.logicalfont('Times', sizeIncr=-1, weight='bold')
+			self.st = Pmw.ScrolledText(self.center_frame,
+			labelpos = 'n',
+			label_text='Program Memory & Opcodes',
+			label_font=('Times', 15),
+
+			usehullsize = 1,
+			hull_width = 465,
+			hull_height = 500,
+			text_wrap='none',
+			text_font = fixedFont,
+
+			text_padx = 4,
+			text_pady = 4,
+			)
+			self.st.importfile('opcode_write.txt');
+			self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
+
+			# Prevent users' modifying text and headers
+			self.st.configure(
+		    	text_state = 'disabled',
+
+			)
+		
+			### Bottom Frame Contents: Registers and Memory Contents" 
+			fixedFont = Pmw.logicalfont('Times', sizeIncr=-1, weight='bold')
+			self.st = Pmw.ScrolledText(self.bottom_frame,
+			labelpos = 'n',
+			label_text='Others',
+			label_font=('Times', 15),
+
+			usehullsize = 1,
+			hull_width = 1400,
+			hull_height = 500,
+			text_wrap='none',
+			text_font = fixedFont,
+
+			text_padx = 4,
+			text_pady = 4,
+			)
+			self.st.importfile('test.asm');
+			self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
+
+			# Prevent users' modifying text and headers
+			self.st.configure(
+		    	text_state = 'disabled',
+
+			)
+			
+
+	root = Tk()
+	root.title('8051 Simulator')
+	myapp = MyApp(root)
+	root.mainloop()
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 from Read_Code import *
 from runner import *
+from Write_files import *
+from GUI import *
 
 
 Read_Code()
 runner()
+Write_files()
+GUI()
 
