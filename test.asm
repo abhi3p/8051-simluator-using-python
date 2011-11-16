8ccf68e7f7eaad86bb7fae08a41288b0e2afec43
Abhishek Pandey <abhi.pandey03@iitb.ac.in>
2011-11-16 15:33:48 +0530

    Some changes

--- test.asm
+++ test.asm
@@ -1,17 +1,8 @@
 MOV A,#020
 MOV A,#10
-<<<<<<< local
 MOV B,#12
 MOV DPTR,#02343
-=======
 MOV B,#4F
->>>>>>> other
-mov r0,a
-MOV R1,A
-<<<<<<< local
 mov 0f3,#24
 MOV A,0f3
-=======
-MOV A,#FF
-MOV A,@R0
->>>>>>> other
+
