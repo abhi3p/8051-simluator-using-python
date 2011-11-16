f5fac6871541067c026932e5bb2c8fe00ef3b6e5
sameerhiware <sameerhiware@localhost>
2011-11-16 20:30:46 +0530

    MAJOR commit for Read_Code.py to generate opcode :) :) :)

--- Read_Code.py
+++ Read_Code.py
@@ -13,23 +13,24 @@ def Read_Code(filename):
 	print asm_code_list
 	
 	f.close()
-	opcode=[]
+	opcodes=[]
 	
-	count=0
-	for n in asm_code_list:
-		if (asm_code_list[count]==''):
-			pass
-		else:
-			opcode+=decode(n,len(opcode),count+1)
-		if UC.flag == 1:
-			break		
-		count=count+1				
-	print opcode
+#	count=0
+#	for n in asm_code_list:
+#		if (asm_code_list[count]==''):
+#			pass
+#		else:
+#			opcode+=decode(n,len(opcode),count+1)
+#		if UC.flag == 1:
+#			break		
+#		count=count+1
+	opcodes = decoderinst(asm_code_list)
+	print opcodes
 	
-	for n in range(0,len(opcode)):
-		UC.ROM[n]=opcode[n]
+	for n in range(0,len(opcodes)):
+		UC.ROM[n]=opcodes[n]
 
-	UC.PC=len(opcode)
+	UC.PC=len(opcodes)
 	print UC.ROM[0:20]
 	print UC.PC
 	
